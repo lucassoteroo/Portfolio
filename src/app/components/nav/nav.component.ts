34782b99ea85365cf4deccabaceab78344af5a51
lucassoteroo <lucassoteroo14@gmail.com>
2022-09-20 22:11:51 -0300

    criando responsividade mobile para tela inicial

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  public valor: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    console.log('teste')
+    if (this.valor === true) {
+      this.valor = false;
+    } else if (this.valor === false) {
+      this.valor = true;
+    }
   }
 
 }
