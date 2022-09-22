bd8c7fe80a1df9f39226a65fdeb13a52b053b9a1
lucassoteroo <lucassoteroo14@gmail.com>
2022-09-21 21:14:10 -0300

    organizando componentes de home

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 
 // Components
-import { NavComponent } from './nav/nav.component';
-import { ApresentacaoComponent } from './apresentacao/apresentacao.component';
+import { NavComponent } from './home/nav/nav.component';
+import { ApresentacaoComponent } from './home/apresentacao/apresentacao.component';
 
 
 @NgModule({
