2dbdb8322137fe31aa3a9410f46f0b12145f029c
lucassoteroo <lucassoteroo14@gmail.com>
2022-12-08 22:59:49 -0300

    deploy da aplicação no github 2.1

--- src/app/services/database-certificados.service.ts
+++ src/app/services/database-certificados.service.ts
@@ -7,27 +7,27 @@ export class DatabaseCertificadosService {
   private certificadosView: any = [
     {
       sobre: 'Javascript',
-      image: '../../../assets/images/certificados/programacaoParaInternetComJs.jpg',
+      image: './assets/images/certificados/programacaoParaInternetComJs.jpg',
     },
     {
       sobre: 'Javascript',
-      image: '../../../assets/images/certificados/jsEs6Essencial.jpg',
+      image: './assets/images/certificados/jsEs6Essencial.jpg',
     },
     {
       sobre: 'Typescript',
-      image: '../../../assets/images/certificados/dominandoTypescript.jpg',
+      image: './assets/images/certificados/dominandoTypescript.jpg',
     },
     {
       sobre: 'Angular',
-      image: '../../../assets/images/certificados/angularAvancado(v13)typescriptAvancado.jpg',
+      image: './assets/images/certificados/angularAvancado(v13)typescriptAvancado.jpg',
     },
     {
       sobre: 'Docker',
-      image: '../../../assets/images/certificados/dockerFerramentaEssencialParaDesenvolvedores.jpg',
+      image: './assets/images/certificados/dockerFerramentaEssencialParaDesenvolvedores.jpg',
     },
     {
       sobre: 'MySQL',
-      image: '../../../assets/images/certificados/bancoDeDadosSqlDoBasicoAoAvancado.jpg',
+      image: './assets/images/certificados/bancoDeDadosSqlDoBasicoAoAvancado.jpg',
     },
   ]
 
